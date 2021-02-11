f3fc64c29cab1758088871a54470f1595ef14a35
shaw8wit <shaw8wit@gmail.com>
2021-02-11 12:07:46 +0530

    added editable template

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ const caption = document.getElementById('caption');
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    scrollFunction()
-};
+window.onscroll = scrollFunction;
 
 function scrollFunction() {
     if (document.body.scrollTop > 420 || document.documentElement.scrollTop > 420) {
@@ -26,49 +24,66 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+const getMemeBody = (name, caption, url, id, isEditable) => {
+    return `<header class="card-header">
+                <h3 class="card-header-title is-size-5">${name}</h3>
+            </header>
+            <div class="card-content">
+                <div class="content">` + (
+        isEditable ?
+        `<div class="field">
+            <label class="label" for="caption">Caption</label>
+                <div class="control">
+                    <input pattern="[a-zA-Z ]{2,}" type="text" name="caption" id="caption"
+                        class="has-text-centered input" placeholder="Be creative with the caption"
+                        autocomplete="off" value="${caption}">
+                </div>
+            </div>
+        <div class="field">
+            <label class="label" for="url">Meme URL</label>
+                <div class="control">
+                    <input type="url" name="url" id="url" class="has-text-centered input"
+                        placeholder="Enter URL of your meme here" autocomplete="off" value="${url}">
+                </div>
+        </div>` :
+        `<h1 class="subtitle is-size-5-tablet is-size-6-mobile m-0 has-text-weight-normal">${caption}</h1>`
+    ) + `</div>
+                    </div>` + (
+        isEditable ?
+        `` :
+        `<div class="card-image">
+            <figure class="image is-5by4">
+                <img src="${url}" alt="${name} : ${caption}">
+            </figure>
+        </div>`
+    ) + `
+            <footer class="card-footer">
+                <button onclick="editMeme(${id})" class="button ` + (
+        isEditable ?
+        'is-link is-outlined' :
+        'is-primary is-light'
+    ) + ` card-footer-item">
+                    <span class="icon">
+                        <i class="fa fa-` + (isEditable ? 'save' : 'edit') + `"></i>
+                    </span>&nbsp;
+                    ` + (isEditable ? 'Save' : 'Edit') + `
+                </button>
+            </footer>`;
+}
+
 const getMemeHolder = (name, caption, url, id) => {
     return `<div class="column is-4-desktop is-6-tablet">
                 <div class="card meme" id="${id}">
-                    <header class="card-header">
-                        <h3 class="card-header-title is-size-5">${name}</h3>
-                    </header>
-                    <div class="card-content">
-                        <div class="content">
-                            <h1 class="subtitle is-size-5-tablet is-size-6-mobile m-0 has-text-weight-normal">${caption}</h1>
-                        </div>
-                    </div>
-                    <div class="card-image">
-                        <figure class="image is-5by4">
-                            <img src="${url}" alt="${name}:${caption}">
-                        </figure>
-                    </div>
-                    <footer class="card-footer">
-                        <button onclick="editMeme(${id})" class="button card-footer-item">
-                            <span class="icon">
-                                <i class="fa fa-edit"></i>
-                            </span>&nbsp;
-                            Edit
-                        </button>
-                    </footer>
+                    ` + getMemeBody(name, caption, url, id, false) + `
                 </div>
             </div>`;
 }
 
 const editMeme = (e) => {
     const meme = document.getElementById(e);
-    const button = meme.querySelector('.button');
-    if (button.querySelector('.icon .fa').classList.contains('fa-edit')) {
-        button.innerHTML = `<span class="icon">
-                                <i class="fa fa-save"></i>
-                            </span>&nbsp;
-                            Save`;
-    } else {
-        button.innerHTML = `<span class="icon">
-                                <i class="fa fa-edit"></i>
-                            </span>&nbsp;
-                            Edit`;
-    }
-    button.classList.toggle('is-dark');
+    if (meme.querySelector('.button .icon .fa').classList.contains('fa-edit')) {
+        meme.innerHTML = getMemeBody()
+    } else {}
 }
 
 const checkImage = (url) => {
@@ -112,7 +127,7 @@ const saveMeme = () => {
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            // console.log(data);
             getMemes();
         });
 
